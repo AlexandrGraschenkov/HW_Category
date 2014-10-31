6bb9eaf3a8d2893d9f8a421e4a5255f2c874654c
Михаил <mihail@MacBook-Air-Mihail.local>
2014-10-31 20:11:43 +0300

    homework2

--- Calculator/NSArray+Ex.m
+++ Calculator/NSArray+Ex.m
@@ -10,7 +10,80 @@
 
 @implementation NSArray (Ex)
 
+- (NSInteger)countNSStringElements
+{
+    NSInteger count = 0;
+    for (NSString *string in self) {
+        if ([string isKindOfClass: [NSString class]])
+            count++;
+    }
+    return count;
+}
 
+- (NSString *)concatAllStringsInArray
+{
+    NSMutableString *concat = [NSMutableString new];
+    for (NSString *string in self) {
+        if ([string isKindOfClass: [NSString class]])
+            concat = [concat stringByAppendingString:string];
+    }
+    return concat;
+}
+
+- (NSNumber *)maxNumberInArray
+{
+    NSNumber* max;
+    if (self.count == 0){
+        max = nil;
+    } else {
+        max = self[0];
+        for (NSNumber *i in self){
+            if ([i floatValue] > [max floatValue]){
+                max = i;
+            }
+        }
+    }
+    return max;
+}
+
+
+- (NSArray *)substractArray:(NSArray *)arr
+{
+    NSMutableArray* array = [NSMutableArray new];
+    for (int i = 0; i < self.count; i++){
+        if (![arr containsObject:self[i]]){
+            [array addObject:self[i]];
+        }
+    }
+    return array;
+}
+
++ (NSArray *)numbersFrom:(NSInteger)fromVal toValue:(NSInteger)toValue
+{
+    NSInteger length = abs(toValue - fromVal) + 1;
+    NSMutableArray* array = [[NSMutableArray alloc] initWithCapacity:length];
+    if (fromVal < toValue) {
+        for (int i = fromVal; i <= toValue; i++) {
+            [array addObject:@(i)];
+        }
+    } else {
+        for (int i = fromVal; i >= toValue; i--) {
+            [array addObject:@(i)];
+        }
+    }
+    return array;
+}
+
++ (NSArray *)charactersInString:(NSString *)str
+{
+    NSMutableArray* array = [[NSMutableArray alloc] initWithCapacity:str.length];
+    for (NSInteger i = 0; i < str.length; i++) {
+        //NSString* chars  = [NSString stringWithFormat: @"%c",[str characterAtIndex: i]];
+        //[array addObject: chars];
+        [array addObject:[str substringWithRange:NSMakeRange(i,1)]];
+    }
+    return array;
+}
 @end
 
 
