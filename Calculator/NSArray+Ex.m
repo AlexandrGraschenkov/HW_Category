f537372846c133e4f79407d9df8ec621f25709f2
itisioslab <itisioslab@jurm-017.i.ksu.ru>
2014-11-05 11:48:12 -0800

    Я сделал вторую домашку! :-)

--- Calculator/NSArray+Ex.m
+++ Calculator/NSArray+Ex.m
@@ -10,7 +10,67 @@
 
 @implementation NSArray (Ex)
 
+- (NSInteger)countNSStringElements {
+    NSInteger count=0;
+    for (NSString *str in self) {
+        if ([str isKindOfClass: [NSString class]])
+            count++;
+    }
+    return count;
+}
 
+- (NSString *)concatAllStringsInArray {
+    NSMutableString *con = [NSMutableString new];
+    for (NSString *str in self) {
+        if ([str isKindOfClass: [NSString class]])
+            con=[con stringByAppendingString:str];
+    }
+    return con;
+}
+
+- (NSNumber *)maxNumberInArray {
+    NSNumber* max;
+    if (self.count==0){
+        max = nil;
+    } else {
+        max=self[0];
+        for (NSNumber *i in self){
+            if ([i floatValue] > [max floatValue]){
+                max=i;
+            }
+        }
+    }
+    return max;
+}
+
+- (NSArray *) substractArray:(NSArray *) arr {
+    NSMutableArray* arr1 = [NSMutableArray new];
+    for (int i = 0; i < self.count; i++){
+        if (![arr containsObject:self[i]]){
+            [arr1 addObject:self[i]];
+        }
+    }
+    return arr1;
+}
+
++ (NSArray *) numbersFrom:(NSInteger)fromVal toValue:(NSInteger)toValue {
+    NSInteger length = abs(toValue) +1;
+    NSMutableArray *arr = [[NSMutableArray alloc]initWithCapacity:length];
+    if (fromVal < toValue) {
+        for (int i = fromVal; i<= toValue; i++) {
+            [arr addObject:@(i)];
+        }
+    }
+    return arr;
+}
+
++(NSArray *) charactersInString:(NSString *)str {
+    NSMutableArray *arr = [[NSMutableArray alloc]initWithCapacity:str.length];
+    for (NSInteger i=0; i<str.length; i++) {
+        [arr addObject:[str substringWithRange:NSMakeRange(i, 1)]];
+    }
+    return arr;
+}
 @end
 
 
